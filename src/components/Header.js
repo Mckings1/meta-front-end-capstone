b4f27e618c2b5cef23ea5d0ce89ad7bb3dcecf1b
oluwasegun michael <alabioluwasegun8@gmail.com>
2024-10-08 19:51:07 +0100

    metaindexhtmlworkedon

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header>
       <h1>Little Lemon</h1>
-      <img src="" alt="Little Lemon Logo" />
+      <img src="images/" alt="Little Lemon Logo" />
     </header>
   );
 }

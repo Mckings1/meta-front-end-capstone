d1917be81ead45da66e60fca709d9da13688a5c1
oluwasegun michael <alabioluwasegun8@gmail.com>
2024-10-12 17:05:02 +0100

    addedScript

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,8 @@
 import React from "react";
+<Routes>
+   <Route path="/" element={<HomePage />}></Route>
+  <Route path="/booking" element={<BookingPage />}></Route>
+</Routes>;
 
 function Main() {
   return <main className="header">Homepage</main>;
